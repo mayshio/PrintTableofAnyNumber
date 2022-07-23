bc0e4204250f1d5957507d91f4600d7be394b747
mayshio <109810450+mayshio@users.noreply.github.com>
2022-07-23 13:36:57 +0530

    Initial Commit

---
+++ ForLoop/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ForLoop
+//
+//  Created by Shio on 23/07/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
